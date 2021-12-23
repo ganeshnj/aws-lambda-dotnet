643fba29ee4a27773cebe7ce5c05bdfac1ed46ad
Ganesh Jangir <jangirg@amazon.com>
2021-12-23 10:16:27 -0800

    update description of pR

--- LambdaRuntimeDockerfiles/update-dockerfile.ps1
+++ LambdaRuntimeDockerfiles/update-dockerfile.ps1
@@ -1,32 +1,52 @@
 param ([Parameter(Mandatory)]$Dockerfile)
 
+$ErrorActionPreference = "Suspend"
+
 # Updates the Dockerfile with next ASP.NET Core version and sha256 hash if available
 function Update-Dockerfile ([string]$path) {
     $nextVersion = Get-NextASPNETVersion -Dockerfile $path
 
     $shaFilePath = "${nextVersion}-sha.txt"
 
-    Invoke-WebRequest -Uri "https://dotnetcli.blob.core.windows.net/dotnet/checksums/${nextVersion}-sha.txt" -OutFile $shaFilePath
+    $sha512Uri = "https://dotnetcli.blob.core.windows.net/dotnet/checksums/${nextVersion}-sha.txt"
+
+    Invoke-WebRequest -Uri $sha512Uri -OutFile $shaFilePath
     if (!$?) {
         Write-Host "Failed to download checksums for ${nextVersion}. ${nextVersion} is not available yet."
         return
     }
 
-    $sha512 = Get-SHA512 -Version $nextVersion -Path $shaFilePath
+    $arch = Get-Architecture -Dockerfile $path
+
+    $artifact = "aspnetcore-runtime-${nextVersion}-linux-${arch}.tar.gz"
+    $sha512 = Get-SHA512 -Artifact $artifact -Path $shaFilePath
+
     (Get-Content $path) -replace 'ARG ASPNET_VERSION=.*', "ARG ASPNET_VERSION=${nextVersion}" -replace 'ASPNET_SHA512=.*', $sha512 | Out-File $path
 
-    Write-Host "Updated ${path} Dockerfile to ${nextVersion}."
+    Write-Host "Updated ${path} to ${nextVersion}."
+
+    # This allows sharing the $path variable between steps
+    # which is needed to update the description of the PR
+    Write-Host "::set-output name=${path}::Updated ${path} to ${nextVersion}. Artifact: ${artifact}, SHA512 Source: ${sha512Uri}"
 }
 
 # Returns SHA512 of given ASP.NET Core version from the give SHA512 file
-function Get-SHA512 ([string]$version, [string]$path) {
-    $artifact = "aspnetcore-runtime-${version}-linux-x64.tar.gz"
-
+function Get-SHA512 ([string]$artifact, [string]$path) {
     $line = Select-String -Path $path -Pattern $artifact | Select-Object -Property Line -ExpandProperty Line
     Write-Host $line
 
     $sha512 = $line.Split(" ")[0]
     return $sha512
+}
+
+function Get-Architecture ([string]$Dockerfile) {
+    if ($Dockerfile.Contains("amd64")) {
+        return "x64"
+    } elseif ($Dockerfile.Contains("arm64")) {
+        return "arm64"
+    } else {
+        throw "Unsupported architecture"
+    }
 }
 
 # Returns the next ASP.NET version to be updated in the Dockerfile
